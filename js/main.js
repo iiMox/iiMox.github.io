8c405977dc03c04d0d5a96db4e29833ac48d4325
iiMox <iimox04@gmail.com>
2021-08-02 16:04:29 +0100

    Some changes

--- js/main.js
+++ js/main.js
@@ -6,80 +6,80 @@ $("body").niceScroll({
 
 let myHeaderHeight = $(".slider .header").innerHeight();
 
-$(".slider .slider-content").height($(window).height() - (myHeaderHeight + $(".slider hr").outerHeight()));
+$(".slider .slider-content").height(
+    $(window).height() - (myHeaderHeight + $(".slider hr").outerHeight())
+);
 let mySliderHeight = $(".slider .slider-content").height();
 
 $(".slider .slider-content .container").css({
-    'padding-top': (mySliderHeight - $(".slider .slider-content .container").innerHeight()) / 2
+    "padding-top":
+        (mySliderHeight -
+            $(".slider .slider-content .container").innerHeight()) /
+        2,
 });
-
-
 
 let ulHover = $(".slider ul .ul-hover");
 
-
 ulHover.height($(".slider .header .links ul li").innerHeight());
-
 
 $(".links ul li").hover(function () {
     ulHover.stop();
     ulHover.width($(this).innerWidth());
     ulHover.offset({ left: $(this).offset().left });
-    ulHover.animate({
-        bottom: 0,
-    }, 200);
-    $(this).css("color", "#fff")
+    ulHover.animate(
+        {
+            bottom: 0,
+        },
+        200
+    );
+    $(this).css("color", "#fff");
 });
 
 $(".links ul li").click(function () {
     let myUl = $(this);
-    $("html").animate({
-        scrollTop: $("." + myUl.data("scroll")).offset().top
-    }, 500)
-})
+    $("html").animate(
+        {
+            scrollTop: $("." + myUl.data("scroll")).offset().top,
+        },
+        500
+    );
+});
 
 $(".links ul li").mouseout(function () {
     ulHover.stop();
     let myUl = $(this);
-    ulHover.animate({
-        bottom: "-47px",
-    }, 200);
-    myUl.css("color", "#af4eff")
-
+    ulHover.animate(
+        {
+            bottom: "-47px",
+        },
+        200
+    );
+    myUl.css("color", "#af4eff");
 });
 
-let imgHover = $(".gallery .works .img-overflow");
-
-imgHover.height($(".gallery .works .work").height());
-
-imgHover.width($(".gallery .works .work").width());
-
-
-
-$(".gallery .works .work img").each(function () {
+$(".gallery .works .work").each(function () {
     let myWork = $(this);
     myWork.mouseenter(function () {
-        imgHover.offset({
+        /* imgHover.offset({
             top: myWork.offset().top,
             left: myWork.offset().left,
-        });
-        imgHover.show(500, function () {
-            imgHover.mouseleave(function () {
-                imgHover.stop();
-                imgHover.hide();
+        }); */
+        $(this)
+            .find(".img-overflow")
+            .show(500, function () {
+                $(this).mouseleave(function () {
+                    $(this).stop();
+                    $(this).hide();
+                });
             });
-        });
     });
 });
 
-
 $("footer .arrow-up").click(function () {
-    $("html").animate({
-        scrollTop: 0,
-    }, 500)
+    $("html").animate(
+        {
+            scrollTop: 0,
+        },
+        500
+    );
 });
-
-
-
-
-
